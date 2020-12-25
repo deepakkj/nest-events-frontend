10b7723545f9d2650cb848de51a7c32ff517ef91
Piotr Jura <>
2020-12-25 17:53:13 +0100

    Registration page

--- src/Login.vue
+++ src/Login.vue
@@ -3,15 +3,19 @@
     <div class="max-w-md w-2/3 border">
       <form @submit.prevent="signIn">
         <div class="-space-y-px">
-          <div><label for="username" class="sr-only">Login</label><input type="text" id="username"
-                                                                         placeholder="Username"
-                                                                         v-model="credentials.username"
-                                                                         class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+          <div>
+            <label for="username" class="sr-only">Login</label>
+            <input type="text" id="username"
+                   placeholder="Username"
+                   v-model="credentials.username"
+                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
           </div>
-          <div><label for="password" class="sr-only">Password</label><input type="password" id="password"
-                                                                            placeholder="Password"
-                                                                            v-model="credentials.password"
-                                                                            class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input type="password" id="password"
+                   placeholder="Password"
+                   v-model="credentials.password"
+                   class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm">
           </div>
 
           <div class="pt-8">
--- src/Register.vue
+++ src/Register.vue
@@ -42,7 +42,3 @@ export default {
   name: "Register"
 }
 </script>
-
-<style scoped>
-
-</style>
