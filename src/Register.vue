5f167769b7d8f07160926524c064272a2b5c861f
Piotr Jura <>
2020-12-26 13:07:55 +0100

    Registration errors

--- src/Register.vue
+++ src/Register.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="flex justify-center">
     <div class="bg-white mr-4 shadow text-sm rounded-sm max-w-xl w-full">
+      <div v-if="errors.length" class="bg-red-50 p-5 flex rounded-t-sm">
+        <div class="w-8 h-8 mr-4 text-red-700">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+          </svg>
+        </div>
+        <div>
+          <div class="text-red-700 font-semibold mb-2 text-lg">There were some errors</div>
+          <div class="text-red-600 text-xs">
+            <ul class="list-disc list-inside">
+              <li v-for="(error, index) in errors" :key="index">{{ error }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
       <form @submit.prevent="register">
         <div class="grid grid-cols-6 gap-6 p-5">
           <div class="col-span-3">
@@ -34,7 +51,7 @@
           </div>
         </div>
 
-        <div class="col-span-6 flex justify-end bg-gray-100 p-5">
+        <div class="col-span-6 flex justify-end bg-gray-100 p-5 rounded-b-sm">
           <ButtonSubmitIndigo label="Register"></ButtonSubmitIndigo>
         </div>
 
@@ -61,14 +78,21 @@ export default {
       firstName: null,
       lastName: null
     });
+    const errors = ref([]);
     const register = async () => {
-      (await api.post(`/users`, userData.value)).data;
-      await router.push({
-        name: 'event-list'
-      });
+      errors.value = [];
+
+      try {
+        (await api.post(`/users`, userData.value)).data;
+        await router.push({name: 'event-list'});
+      } catch (e) {
+        if (400 === e.response?.status) {
+          errors.value = e.response.data.message;
+        }
+      }
     }
 
-    return {userData, register};
+    return {userData, register, errors};
   }
 }
 </script>
